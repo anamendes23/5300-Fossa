e048c354f796c6bbc7485c1ad21d689f325302d5
FangshengXuJason <xufangsheng@seattleu.edu>
2022-04-11 20:12:33 -0700

    parital implementation for slottedPage in heap_storage.cpp

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -1,23 +1,47 @@
 #include "heap_storage.h"
-#include "storage_engine.h"
-bool test_heap_storage() {return true;}
+/*
+* Naive Test from Kevin
+*/
+bool test_heap_storage();
 
 /*
  * Convenient aliases for types
  */
+ // Copied them from storage_engine.h
 typedef u_int16_t RecordID;
 typedef u_int32_t BlockID;
 typedef std::vector<RecordID> RecordIDs;
 typedef std::length_error DbBlockNoRoomError;
 
+// copied from instructor's code
+typedef u_int16_t u16;
+
 SlottedPage::SlottedPage(Dbt &block, BlockID block_id, bool is_new) : DbBlock(block, block_id) {
+    if (is_new) {
+        this->num_records = 0;
+        this->end_free = DbBlock::BLOCK_SZ - 1;
+        put_header();
+    } else {
+        get_header(this->num_records, this->end_free);
+    }
 }
 
+// not sure if we need it
 // SlottedPage::~SlottedPage() {
 // }
 
-RecordID SlottedPage:: add(const Dbt *data) {
-    return 0;
+RecordID SlottedPage:: add(const Dbt* data) {
+    if (!has_room(data->get_size()))
+        throw DbBlockNoRoomError("not enough room for new record");
+    u16 id = ++this->num_records;
+    u16 size = (u16) data->get_size();
+    this->end_free -= size;
+    u16 loc = this->end_free + 1;
+    put_header();
+    put_header(id, size, loc);
+    // memory-to-memory copy
+    std::memcpy(this->address(loc), data->get_data(), size);
+    return id;
 }
 
 Dbt* SlottedPage::get(RecordID record_id) {
@@ -44,8 +68,14 @@ void SlottedPage::get_header(u_int16_t &size, u_int16_t &loc, RecordID id) {
 
 }
 
-void SlottedPage::put_header(RecordID id, u_int16_t size, u_int16_t loc) {
-
+// Store the size and offset for given id. For id of zero, store the block header.
+void SlottedPage::put_header(RecordID id, u16 size, u16 loc) {
+    if (id == 0) { // called the put_header() version and using the default params
+        size = this->num_records;
+        loc = this->end_free;
+    }
+    put_n(4*id, size);
+    put_n(4*id + 2, loc);
 }
 
 bool SlottedPage::has_room(u_int16_t size) {
@@ -56,14 +86,59 @@ void SlottedPage::slide(u_int16_t start, u_int16_t end) {
 
 }
 
-u_int16_t SlottedPage::get_n(u_int16_t offset) {
-    return 0;
+// Get 2-byte integer at given offset in block.
+u16 SlottedPage::get_n(u16 offset) {
+    return *(u16*)this->address(offset);
 }
 
-void SlottedPage::put_n(u_int16_t offset, u_int16_t n) {
-
+// Put a 2-byte integer at given offset in block.
+void SlottedPage::put_n(u16 offset, u16 n) {
+    *(u16*)this->address(offset) = n;
 }
 
-void* SlottedPage::address(u_int16_t offset) {
-    return 0;
-};
+// Make a void* pointer for a given offset into the data block.
+void* SlottedPage::address(u16 offset) {
+    return (void*)((char*)this->block.get_data() + offset);
+}
+
+// test function -- returns true if all tests pass
+bool test_heap_storage() {
+	ColumnNames column_names;
+	column_names.push_back("a");
+	column_names.push_back("b");
+	ColumnAttributes column_attributes;
+	ColumnAttribute ca(ColumnAttribute::INT);
+	column_attributes.push_back(ca);
+	ca.set_data_type(ColumnAttribute::TEXT);
+	column_attributes.push_back(ca);
+    HeapTable table1("_test_create_drop_cpp", column_names, column_attributes);
+    table1.create();
+    std::cout << "create ok" << std::endl;
+    table1.drop();  // drop makes the object unusable because of BerkeleyDB restriction -- maybe want to fix this some day
+    std::cout << "drop ok" << std::endl;
+
+    HeapTable table("_test_data_cpp", column_names, column_attributes);
+    table.create_if_not_exists();
+    std::cout << "create_if_not_exsts ok" << std::endl;
+
+    ValueDict row;
+    row["a"] = Value(12);
+    row["b"] = Value("Hello!");
+    std::cout << "try insert" << std::endl;
+    table.insert(&row);
+    std::cout << "insert ok" << std::endl;
+    Handles* handles = table.select();
+    std::cout << "select ok " << handles->size() << std::endl;
+    ValueDict *result = table.project((*handles)[0]);
+    std::cout << "project ok" << std::endl;
+    Value value = (*result)["a"];
+    if (value.n != 12)
+    	return false;
+    value = (*result)["b"];
+    if (value.s != "Hello!")
+		return false;
+    table.drop();
+
+    return true;
+}
+
